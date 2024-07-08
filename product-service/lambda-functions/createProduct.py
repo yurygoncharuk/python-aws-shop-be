ce0ccae3245f830d90683aa4cc7828373c435d61
Yury Hancharuk <yury.goncharuk@gmail.com>
2024-07-08 19:15:07 +0200

    [task-6] small improvements for product service

--- product-service/lambda-functions/createProduct.py
+++ product-service/lambda-functions/createProduct.py
@@ -1,13 +1,24 @@
 import boto3
 import json
-from common import write_to_dynamodb 
+from common import write_to_dynamodb, return_message 
 
 def handler(event, lambda_context):
-    print(json.dumps(event))
-    product = json.loads(event['body'])
-    dynamodb = boto3.client('dynamodb')
-    response = write_to_dynamodb(dynamodb, product)
-    return response
+    try:
+        print(json.dumps(event))
+        product = json.loads(event['body'])
+        dynamodb = boto3.client('dynamodb')
+        response = write_to_dynamodb(dynamodb, product)
+        return response
+    
+    except KeyError as ke:
+        # Handle missing key errors
+        print(f"Error: {ke}")
+        return return_message(400, {"message": f"Missing required key: {ke}"})
+
+    except Exception as e:
+        # Handle generic exceptions
+        print(f"Error: {e}")
+        return return_message(500, {"message": f"Failed to create new product: {e}"})
     
 if __name__ == "__main__":
     handler(None, None)
